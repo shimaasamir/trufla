0114307a27bd8faf971fbdc8b65980534e0d959d
shimaasamir <shimaasamir88@hotmail.com>
2019-06-03 17:14:31 +0200

    add angular material data table with pagination and sorting

---
+++ src/app/data-table/data-table.component.ts
@@ -0,0 +1,27 @@
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, MatSort, MatTable } from '@angular/material';
+import { DataTableDataSource, DataTableItem } from './data-table-datasource';
+
+@Component({
+  selector: 'app-data-table',
+  templateUrl: './data-table.component.html',
+  styleUrls: ['./data-table.component.scss']
+})
+export class DataTableComponent implements AfterViewInit, OnInit {
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  @ViewChild(MatTable, { static: true }) table: MatTable<DataTableItem>;
+  dataSource: DataTableDataSource;
+
+  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+  displayedColumns = ['id', 'name'];
+
+  ngOnInit() {
+    this.dataSource = new DataTableDataSource();
+  }
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.table.dataSource = this.dataSource;
+  }
+}
